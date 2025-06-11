b768086625b9e552b4bcd7a65b500fe26f9adfcd
Paul Magister <pamagister@protonmail.com>
2025-06-11 17:13:55 +0200

    get mkdocs running

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../mbox_converter"))
+sys.path.insert(0, os.path.abspath("../mbox_converter"))
 
 project = "mbox_converter"
 author = "pamagister"
